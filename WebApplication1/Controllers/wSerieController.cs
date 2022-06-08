e97c362677416e301fa04abf4e8bcf0d534c07f9
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2022-06-09 06:32:28 +1200

    Add project files.

---
+++ WebApplication1/Controllers/wSerieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class wSerieController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetSerie()
+        {
+            return Ok();
+        }
+    }
+}
